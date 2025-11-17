912edddee6c92dd489c003efb69c4f1b4598cd1f
Leo Saban <leodsaban@gmail.com>
2025-11-16 19:12:23 -0700

    created the initial look with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+  <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+    <div className="w-full max-w-[400px] mx-5">
+
+      <h1 className="text-5xl font-semibold text-white mb-8 text-center">Tic Tac Toe</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        game status
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="grid grid-col-3 gap-1 rounded-xl overflow-hidden mb-6">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <button className="w-full py-3 text-lg text-white border rounded-xl hover:bg-gray-50 hover:text-gray-800 transition-colors duration-200">New Game</button>
+    </div>
+  </div>
   )
 }
-
-export default App
